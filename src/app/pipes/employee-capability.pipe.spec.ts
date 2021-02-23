f77b22b6f41f979b3a84f33c66a43cde8851003e
ravitiwar <32223098+ravitiwar@users.noreply.github.com>
2021-02-23 11:15:32 +0000

    Deploying to master from @ ravitiwar/People-Portal-Frontend@e4fa4eba21981487e5d23e0adee801a6e8ac47ef 🚀

--- src/app/pipes/employee-capability.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EmployeeCapabilityPipe } from './employee-capability.pipe';
-
-describe('EmployeeCapabilityPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EmployeeCapabilityPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
